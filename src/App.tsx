4d42597a47fd104410724ff21afa116a6482167e
Willy Samuel Paz Colque <willypaz243@gmail.com>
2024-03-26 23:31:25 -0400

    hash routes fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Students } from "./views/students";
 import Teachers from "./views/teachers";
 
 function App() {
   return (
-    <>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/#/students/*" element={<Students />} />
-        <Route path="/#/teachers/*" element={<Teachers />} />
+        <Route path="/students/*" element={<Students />} />
+        <Route path="/teachers/*" element={<Teachers />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
-    </>
+    </HashRouter>
   );
 }
 
