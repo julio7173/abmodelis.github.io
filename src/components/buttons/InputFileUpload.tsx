fad0fefb8d9adc5ccc5d93d7f3d6672045f50113
Julio Cesar Severiche Orellana <39506930+julio7173@users.noreply.github.com>
2024-03-29 18:42:41 -0400

    Update InputFileUpload.tsx

--- src/components/buttons/InputFileUpload.tsx
+++ src/components/buttons/InputFileUpload.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+/*import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -88,3 +88,4 @@ export default function InputFileUpload() {
     </BoxContainer>
   );
 }
+*/
